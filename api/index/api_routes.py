89c6eaf10ad15a8d9e114d674b584838823c34e6
maxmarzolf <max@treesfromspace.com>
2022-01-29 00:16:54 -0300

    Netlix notebook

--- api/index/api_routes.py
+++ api/index/api_routes.py
@@ -1,8 +1,8 @@
 from . import api
-from api import session
+from api import session, engine
 from api.models import NetflixOriginal
 from flask import jsonify
-from sqlalchemy import desc
+from sqlalchemy import desc, text
 
 
 @api.route("/netflix_originals/all", methods=['GET'])
@@ -16,12 +16,48 @@ def all_movies():
     return status
 
 
+@api.route("/netflix_originals/all/titles", methods=['GET'])
+def all_titles():
+    try:
+        sql = 'select title from netflix_originals'
+        result = engine.execute(sql)
+        titles = [row[0] for row in result]
+        return jsonify(titles)
+    except Exception as e:
+        status = "Error! = " + str(e)
+    return status
+
+
 @api.route("/netflix_originals/highest_rated", methods=['GET'])
 def highest_rated():
     try:
         result = session.query(NetflixOriginal).order_by(desc(NetflixOriginal.imdb_score)).limit(1).all()
         format_result = [r.serialize() for r in result]
         return jsonify(format_result)
+    except Exception as e:
+        status = "Error! = " + str(e)
+    return status
+
+
+@api.route("/netflix_originals/longest", methods=['GET'])
+def longest():
+    try:
+        result = session.query(NetflixOriginal).order_by(desc(NetflixOriginal.runtime)).limit(10).all()
+        format_result = [r.serialize() for r in result]
+        return jsonify(format_result)
+    except Exception as e:
+        status = "Error! = " + str(e)
+    return status
+
+
+@api.route("/watched/<movie>/<rating>", methods=['POST'])
+def record_watched(movie, rating):
+    try:
+        query = text(f"insert into watched (title, my_rating) values ('{movie}',{rating})")
+        session.execute(query)
+        session.commit()
+        status = f'You gave {movie} a score of {rating}'
+        return status
     except Exception as e:
         status = "Error! = " + str(e)
     return status
